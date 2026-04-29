ae56d5128dbd00f96924b39f94e6a6c555b18465
duvanrohe04-dotcom <duvanrohe04@gmail.com>
2026-04-28 22:55:15 -0500

    uu
    uu

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory
 from app.models import Service, Staff, Appointment
+import os
 
 main_bp = Blueprint('main', __name__)
 
@@ -11,7 +12,19 @@ def index():
     barbers = Staff.query.filter_by(gender='male', active=True).all()
     stylists = Staff.query.filter_by(gender='female', active=True).all()
     return render_template('index.html',
-                           male_services=male_services,
-                           female_services=female_services,
-                           barbers=barbers,
-                           stylists=stylists)
+                            male_services=male_services,
+                            female_services=female_services,
+                            barbers=barbers,
+                            stylists=stylists)
+
+
+@main_bp.route('/download/android')
+def download_android():
+    """Descarga la app para Android (APK)."""
+    return send_from_directory('static/downloads', 'app-android.apk', as_attachment=True)
+
+
+@main_bp.route('/download/pc')
+def download_pc():
+    """Descarga la app para PC (ZIP o EXE)."""
+    return send_from_directory('static/downloads', 'app-windows.zip', as_attachment=True)
